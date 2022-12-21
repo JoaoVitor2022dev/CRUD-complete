1d6b8dceaa2b50413bc991f818ca2afebfcf21fd
Joao Vitor Mocambite Lima <86741407+JoaoVitor2022dev@users.noreply.github.com>
2022-12-21 17:23:01 -0400

    Add files via upload

---
+++ CRUD- completo/client/src/cards/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./cards/Card.css";
+
+const Card = (props) => {
+  return (
+    <div className="card--container">
+        <h1 className="card--title">{props.name}</h1>
+        <p className='card--category'>{props.category}</p>
+        <p className="card--cost">R$ {props.cost}</p>
+    </div>
+  )
+}
+
+export default Card; 
